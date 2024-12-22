c7efd3db7ef627a6ab758a17d0e86970389ffc1f
davidly <davidly_lee@hotmail.com>
2024-12-22 06:52:55 -0800

    cleanup build script

--- m.bat
+++ m.bat
@@ -9,7 +9,4 @@ ntvcm ..\ntvcm\aztec\\cc -DNDEBUG -DAZTECCPM -Y256 -Q -T -F m6502.c
 ntvcm ..\ntvcm\aztec\as -L a1.asm
 ntvcm ..\ntvcm\aztec\as -L m6502.asm
 ntvcm ..\ntvcm\aztec\ln -T a1.o m6502.o m.lib c.lib
-goto :done
-
-:done
 
