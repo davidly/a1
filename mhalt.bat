bbe409c490b69814795b9d9b370dcffbd1c6282f
davidly <davidly_lee@hotmail.com>
2024-12-26 17:35:39 -0800

    use built-in optimizer for a1 and native optimizer for m6502

--- mhalt.bat
+++ mhalt.bat
@@ -1,10 +1,11 @@
-ntvcm -c c309.com -C -DNDEBUG -DHISOFTCPM -S -V -X a1.c
-optim.exe a1.as a1.asm
+rem use the built-in optim.com since it works for a1
+ntvcm -c c309.com -O -C -DNDEBUG -DHISOFTCPM -S -V -X a1.c
+ntvcm zas A1.ASM
 
+rem use the native-built optim.exe for m6502.c since emulate() is large
 ntvcm -c c309.com -C -DNDEBUG -DHISOFTCPM -S -V -X m6502.c
 optim.exe m6502.as m6502.asm
-
-ntvcm zas A1.ASM
 ntvcm zas M6502.ASM
+
 ntvcm linq -MA1.MAP -X -C100H -OA1.COM CRTCPM.OBJ A1.OBJ M6502.OBJ LIBC.LIB
 
