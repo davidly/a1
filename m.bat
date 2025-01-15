2e41abd18c60466efaf5fb6d0c639c6d1b25b937
davidly <davidly_lee@hotmail.com>
2025-01-15 08:38:38 -0800

    fix (harmless) typo in aztec build script

--- m.bat
+++ m.bat
@@ -5,7 +5,7 @@ del m6502.o 2>nul
 del a1.com 2>nul
 
 ntvcm ..\ntvcm\aztec\cc -DNDEBUG -DAZTECCPM -Q -T -F a1.c
-ntvcm ..\ntvcm\aztec\\cc -DNDEBUG -DAZTECCPM -Y256 -Q -T -F m6502.c
+ntvcm ..\ntvcm\aztec\cc -DNDEBUG -DAZTECCPM -Y256 -Q -T -F m6502.c
 ntvcm ..\ntvcm\aztec\as -L a1.asm
 ntvcm ..\ntvcm\aztec\as -L m6502.asm
 ntvcm ..\ntvcm\aztec\ln -T a1.o m6502.o m.lib c.lib
